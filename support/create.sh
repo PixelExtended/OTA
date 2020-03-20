09e0ebc24ef2e386917cd2f81e0baf80dd94ab1f
niteshkumar2000 <nitesh156200@gmail.com>
2020-03-20 13:33:01 +0000

    OTA: added code for moving changelog

--- support/create.sh
+++ support/create.sh
@@ -16,3 +16,4 @@ MAINTAINER_URL="https:\/\/t.me/Niteshkumar15"
 FORUM_URL="https://forum.xda-developers.com/redmi-note-6-pro/development/rom-pixel-experience-t4047809"
 JSON_FMT='{\n"error":false,\n"filename": %s,\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s",\n"donate_url": "%s",\n"website_url":"%s",\n"news_url":"%s",\n"maintainer":"%s",\n"maintainer_url":"%s",\n"forum_url":"%s"\n}'
 printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > ~/OTA/builds/twolip.json
+mv ~/peplus/out/target/product/twolip/PixelExperience*.txt ~/OTA/changelogs/twolip/
