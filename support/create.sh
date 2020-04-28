05364c7433e670117b735e8bc91a00cfb86b391d
Aryan Gupta <guptaaryan189@gmail.com>
2020-04-28 18:29:03 +0530

    Refactor bash code for jasmine

--- support/create.sh
+++ support/create.sh
@@ -1,19 +1,19 @@
-ROMDIR=~/peplus
+ROMDIR=~/plus
 cd $ROMDIR
-DATETIME=$(grep "org.pixelexperience.build_date_utc=" out/target/product/tulip/system/build.prop | cut -d "=" -f 2)
-FILENAME=$(find out/target/product/tulip/PixelExperience*.zip | cut -d "/" -f 5)
-ID=$(md5sum out/target/product/tulip/PixelExperience*.zip | cut -d " " -f 1)
+DATETIME=$(grep "org.pixelexperience.build_date_utc=" out/target/product/jasmine_sprout/system/build.prop | cut -d "=" -f 2)
+FILENAME=$(find out/target/product/jasmine_sprout/PixelExperience*.zip | cut -d "/" -f 5)
+ID=$(md5sum out/target/product/jasmine_sprout/PixelExperience*.zip | cut -d " " -f 1)
 FILEHASH=$ID
-SIZE=$(wc -c out/target/product/tulip/PixelExperience*.zip | awk '{print $1}')
-URL1="https://sourceforge.net/projects/pixelexperience-twolip/files/$FILENAME"
+SIZE=$(wc -c out/target/product/jasmine_sprout/PixelExperience*.zip | awk '{print $1}')
+URL1="https://sourceforge.net/projects/private-dir/files/PixelExperience-Fan_Edition/Decrypted/$FILENAME"
 URL=$URL1
 VERSION="10"
-DONATE_URL="http:\/\/bit.ly\/jhenrique09_paypal"
-WEBSITE_URL="https:\/\/download.pixelexperience.org"
-NEWS_URL="https:\/\/t.me\/PixelExperience"
-MAINTAINER="Niteshkumar"
-MAINTAINER_URL="https:\/\/t.me/Niteshkumar15"
-FORUM_URL="https://forum.xda-developers.com/redmi-note-6-pro/development/rom-pixel-experience-t4047809"
+DONATE_URL="https://www.paypal.me/aryan65"
+WEBSITE_URL="https://sourceforge.net/projects/private-dir/files/PixelExperience-Fan_Edition/Decrypted/"
+NEWS_URL="https:\/\/t.me\/pixeljasmine"
+MAINTAINER="Aryan Gupta"
+MAINTAINER_URL="https:\/\/t.me/boozabooza"
+FORUM_URL="#"
 JSON_FMT='{\n"error":false,\n"filename": %s,\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s",\n"donate_url": "%s",\n"website_url":"%s",\n"news_url":"%s",\n"maintainer":"%s",\n"maintainer_url":"%s",\n"forum_url":"%s"\n}'
-printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > ~/OTA/builds/tulip.json
-mv ~/peplus/out/target/product/tulip/PixelExperience*.txt ~/OTA/changelogs/twolip/
+printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > ~/OTA/builds/jasmine_sprout.json
+mv ~/plus/out/target/product/jasmine_sprout/PixelExperience*.txt ~/OTA/changelogs/jasmine_sprout/
