559fe6c6db256fe32d3fd62b636015b8a11c34c0
Aryan Gupta <guptaaryan189@gmail.com>
2020-07-12 15:19:02 +0530

    Test (#4)
    * July Update
    
    * Update PixelExperience_Plus_jasmine_sprout-10.0-20200712-0645-FAN_EDITION.zip.txt

--- support/create.sh
+++ support/create.sh
@@ -1,11 +1,11 @@
 ROMDIR=~/pe
 cd $ROMDIR
-DATETIME=$(grep "org.pixelexperience.build_date_utc=" out/target/product/wayne/system/build.prop | cut -d "=" -f 2)
-FILENAME=$(find out/target/product/wayne/PixelExperience*.zip | cut -d "/" -f 5)
-ID=$(md5sum out/target/product/wayne/PixelExperience*.zip | cut -d " " -f 1)
+DATETIME=$(grep "org.pixelexperience.build_date_utc=" out/target/product/jasmine_sprout/system/build.prop | cut -d "=" -f 2)
+FILENAME=$(find out/target/product/jasmine_sprout/PixelExperience*.zip | cut -d "/" -f 5)
+ID=$(md5sum out/target/product/jasmine_sprout/PixelExperience*.zip | cut -d " " -f 1)
 FILEHASH=$ID
-SIZE=$(wc -c out/target/product/wayne/PixelExperience*.zip | awk '{print $1}')
-URL1="https://sourceforge.net/projects/private-dir/files/PixelExperience-Fan_Edition/wayne/$FILENAME/download"
+SIZE=$(wc -c out/target/product/jasmine_sprout/PixelExperience*.zip | awk '{print $1}')
+URL1="https://sourceforge.net/projects/private-dir/files/PixelExperience-Fan_Edition/jasmine_sprout/$FILENAME/download"
 URL=$URL1
 VERSION="10"
 DONATE_URL="https://www.paypal.me/aryan65"
@@ -15,4 +15,4 @@ MAINTAINER="Aryan Gupta"
 MAINTAINER_URL="https:\/\/t.me/boozabooza"
 FORUM_URL="#"
 JSON_FMT='{\n"error":false,\n"filename": %s,\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s",\n"donate_url": "%s",\n"website_url":"%s",\n"news_url":"%s",\n"maintainer":"%s",\n"maintainer_url":"%s",\n"forum_url":"%s"\n}'
-printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > ~/OTA/builds/wayne.json
+printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > ~/OTA/builds/jasmine_sprout.json
