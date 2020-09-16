e53ac74ecc59e3ef5064fc7f32c71a19579495b8
Christian de la Cruz <chrisdlc119@outlook.com>
2020-09-16 13:00:45 -0500

    Updated parameters and references - ROMDIR param to variable - Added OTADIR variable - Updated path references for JSON and changelog

--- support/ota.sh
+++ support/ota.sh
@@ -1,21 +1,23 @@
 DEVICE=$1
-ROMDIR=~/$2
-MAINTAINER=$3
-MAINTAINER_URL=$4
+MAINTAINER=$2
+MAINTAINER_URL=$3
 
 # Ensures that mandatory parameters are entered
-if [ $# -lt 4 ]; then
+if [ $# -lt 3 ]; then
     echo "Missing mandatory parameters"
     exit 1
 fi
 
 # Ensures that optional parameters are entered
-if [ -z "$5" ]; then
+if [ -z "$4" ]; then
     FORUM_URL=""
 else
-    FORUM_URL=$5
+    FORUM_URL=$4
 fi
 
+# Define ROM and OTA repo directories
+ROMDIR=~/pex
+OTADIR=~/OTA
 cd $ROMDIR
 DATETIME=$(grep "org.pixelexperience.build_date_utc=" out/target/product/$DEVICE/system/build.prop | cut -d "=" -f 2)
 FILENAME=$(find out/target/product/$DEVICE/PixelExtended*.zip | cut -d "/" -f 5)
@@ -28,8 +30,8 @@ DONATE_URL="https://www.paypal.me/aryan65"
 WEBSITE_URL="https://pixelextended.me/"
 NEWS_URL="https:\/\/t.me\/pexupdates"
 JSON_FMT='{\n"error":false,\n"filename": %s,\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s",\n"donate_url": "%s",\n"website_url":"%s",\n"news_url":"%s",\n"maintainer":"%s",\n"maintainer_url":"%s",\n"forum_url":"%s"\n}'
-printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > $ROMDIR/OTA/builds/$DEVICE.json
-echo "Latest JSON file updated in $ROMDIR/OTA/builds/. Please confirm."
+printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > $OTADIR/builds/$DEVICE.json
+echo "Latest JSON file updated in $OTADIR/builds/. Please confirm."
 
-cp out/target/product/$DEVICE/PixelExtended*Changelog.txt $ROMDIR/OTA/changelogs/$DEVICE
-echo "Latest changelog updated in $ROMDIR/OTA/changelogs/$DEVICE/. Please confirm."
+cp out/target/product/$DEVICE/PixelExtended*Changelog.txt $OTADIR/changelogs/$DEVICE
+echo "Latest changelog updated in $OTADIR/changelogs/$DEVICE/. Please confirm."
