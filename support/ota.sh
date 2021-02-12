b545374dfae55dd7597432dcdc681c9652df4d0c
Aryan Gupta <guptaaryan189@gmail.com>
2021-02-12 07:16:05 +0000

    Test

--- support/ota.sh
+++ support/ota.sh
@@ -22,10 +22,10 @@ FILENAME=$(find out/target/product/$DEVICE/PixelExtended*.zip | cut -d "/" -f 5)
 ID=$(md5sum out/target/product/$DEVICE/PixelExtended*.zip | cut -d " " -f 1)
 FILEHASH=$ID
 SIZE=$(wc -c out/target/product/$DEVICE/PixelExtended*.zip | awk '{print $1}')
-URL="https://sourceforge.net/projects/pixelextended/files/$DEVICE/$FILENAME/download"
-VERSION="10"
+URL="https://sourceforge.net/projects/pixelextended/files/Android11/$FILENAME/download"
+VERSION="11"
 DONATE_URL="https://www.paypal.me/aryan65"
 WEBSITE_URL="https://pixelextended.me/"
 NEWS_URL="https:\/\/t.me\/pexupdates"
 JSON_FMT='{\n"error":false,\n"filename": %s,\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s",\n"donate_url": "%s",\n"website_url":"%s",\n"news_url":"%s",\n"maintainer":"%s",\n"maintainer_url":"%s",\n"forum_url":"%s"\n}'
-printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > ~/OTA/builds/$DEVICE.json
+printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > ~/ota/builds/OTA.json
