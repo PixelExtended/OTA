4d5205f07c01b09ec3f8e87a30afdcde552584d4
Aryan Gupta <guptaaryan189@gmail.com>
2021-06-20 13:48:21 +0530

    incl rom dir

--- support/ota.sh
+++ support/ota.sh
@@ -1,4 +1,5 @@
 DEVICE=$1
+ROMDIR=~/$2
 # Ensures that mandatory parameters are entered
 if [ $# -lt 1 ]; then
     echo "Missing mandatory parameters"
@@ -14,12 +15,12 @@ URL="https://sourceforge.net/projects/pixelextended/files/$DEVICE/$FILENAME/down
 VERSION="11"
 JSON_FMT='{\n"error":false,\n"filename": %s,\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s",\n"donate_url": "%s",\n"website_url":"%s",\n"news_url":"%s",\n"maintainer":"%s",\n"maintainer_url":"%s",\n"forum_url":"%s"\n}'
 printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" > OTA/builds/$DEVICE.json
-echo OTA/builds/$DEVICE.json file created
+echo $ROMDIR/OTA/builds/$DEVICE.json file created
 
 BUILD_DATE=$(echo $FILENAME | cut -d "-" -f 3)
 BUILD_YEAR=${BUILD_DATE:0:4}
 BUILD_MONTH=${BUILD_DATE:4:2}
 BUILD_DAY=${BUILD_DATE:6:2}
 CHANGELOG=$(echo "Changelog - $BUILD_YEAR/$BUILD_MONTH/$BUILD_DAY\n")
-printf "$CHANGELOG" > OTA/changelogs/$DEVICE/$FILENAME.txt
-echo OTA/changelogs/$DEVICE/$FILENAME.txt file created
+printf "$CHANGELOG" > $ROMDIR/OTA/changelogs/$DEVICE/$FILENAME.txt
+echo $ROMDIR/OTA/changelogs/$DEVICE/$FILENAME.txt file created
