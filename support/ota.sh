153197f0f055def2d45c7587e7a469ea475ba9ee
Christian de la Cruz <chrisdlc119@outlook.com>
2021-06-24 17:44:09 -0500

    ota: Remove dir param and double quote filename field

--- support/ota.sh
+++ support/ota.sh
@@ -1,10 +1,4 @@
 DEVICE=$1
-ROMDIR=~/$2
-# Ensures that mandatory parameters are entered
-if [ $# -lt 1 ]; then
-    echo "Missing mandatory parameters"
-    exit 1
-fi
 
 DATETIME=$(grep "org.pixelexperience.build_date_utc=" out/target/product/$DEVICE/system/build.prop | cut -d "=" -f 2)
 FILENAME=$(find out/target/product/$DEVICE/PixelExtended*.zip | cut -d "/" -f 5)
@@ -13,14 +7,16 @@ FILEHASH=$(md5sum out/target/product/$DEVICE/PixelExtended*.zip | cut -d " " -f 
 SIZE=$(wc -c out/target/product/$DEVICE/PixelExtended*.zip | awk '{print $1}')
 URL="https://sourceforge.net/projects/pixelextended/files/$DEVICE/$FILENAME/download"
 VERSION="11"
-JSON_FMT='{\n"error":false,\n"filename": %s,\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s"\n}'
+JSON_FMT='{\n"error":false,\n"filename": "%s",\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s"\n}'
+
 printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" > OTA/builds/$DEVICE.json
-echo $ROMDIR/OTA/builds/$DEVICE.json file created
+echo OTA/builds/$DEVICE.json file created
 
 BUILD_DATE=$(echo $FILENAME | cut -d "-" -f 3)
 BUILD_YEAR=${BUILD_DATE:0:4}
 BUILD_MONTH=${BUILD_DATE:4:2}
 BUILD_DAY=${BUILD_DATE:6:2}
 CHANGELOG=$(echo "Changelog - $BUILD_YEAR/$BUILD_MONTH/$BUILD_DAY\n")
-printf "$CHANGELOG" > $ROMDIR/OTA/changelogs/$DEVICE/$FILENAME.txt
-echo $ROMDIR/OTA/changelogs/$DEVICE/$FILENAME.txt file created
+
+printf "$CHANGELOG" > OTA/changelogs/$DEVICE/$FILENAME.txt
+echo OTA/changelogs/$DEVICE/$FILENAME.txt file created "(make sure to update the changelog file)"
