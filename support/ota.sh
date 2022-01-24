5659d5054ff1cb36c29c56f9aaa441740800e1fa
Aryan Gupta <guptaaryan189@gmail.com>
2022-01-24 15:16:47 +0530

    get pex version from props

--- support/ota.sh
+++ support/ota.sh
@@ -9,10 +9,11 @@ ID=$(sha256sum out/target/product/$DEVICE/PixelExtended*.zip | cut -d " " -f 1)
 FILEHASH=$(md5sum out/target/product/$DEVICE/PixelExtended*.zip | cut -d " " -f 1)
 SIZE=$(wc -c out/target/product/$DEVICE/PixelExtended*.zip | awk '{print $1}')
 URL="https://sourceforge.net/projects/pixelextended/files/$DEVICE/$FILENAME/download"
-VERSION="11"
-JSON_FMT='{\n"error":false,\n"filename": "%s",\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s", \n"tg_username": "%s", \n"device_name":"%s", \n"device":"%s", \n"xda_thread":"%s"\n}'
+VERSION="12"
+PEXV="$(grep "org.pex.version=" out/target/product/$DEVICE/system/build.prop | cut -d "=" -f 2")
+JSON_FMT='{\n"error":false,\n"filename": "%s",\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"pexv": "%s", \n"id": "%s", \n"tg_username": "%s", \n"device_name":"%s", \n"device":"%s", \n"xda_thread":"%s"\n}'
 
-printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$TG_USERNAME" "$DEVICE_NAME" "$DEVICE" "$XDA_THREAD" > OTA/builds/$DEVICE.json
+printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" $"PEXV" "$ID" "$TG_USERNAME" "$DEVICE_NAME" "$DEVICE" "$XDA_THREAD" > OTA/builds/$DEVICE.json
 echo OTA/builds/$DEVICE.json file created
 
 BUILD_DATE=$(echo $FILENAME | cut -d "-" -f 3)
