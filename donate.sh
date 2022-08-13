aba7fb4e4af90e263ddea9053b9431dec701b14d
Aryan Gupta <guptaaryan189@gmail.com>
2022-08-13 21:37:59 +0530

    It should work

---
+++ donate.sh
@@ -0,0 +1,19 @@
+##Variables
+LINK="https://www.paypal.com/paypalme/aryan65"
+INLINK="https://heisinbug.tech/pay"
+
+# Format post
+read -r -d '' msg <<EOF
+<b> Liked Our Worked on delivering a usable PE to you XD , then buy us some beer by donating </b>
+
+<b>For International/PayPal Users</b>: <a href='${LINK}'>Click Here</a>
+<b>For Indian Users</b>: <a href='${INLINK}'>Click Here</a>
+
+EOF
+
+# Make a nice post and send it to TG channel
+curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" \
+    -d chat_id="${CHAT_ID}" \
+    -d "disable_web_page_preview=true" \
+    -d "parse_mode=HTML" \
+    -d text="$msg"
