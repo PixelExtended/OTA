308a3d2713c54f317c180700743dad6e89c8bc22
heisinbug <guptaaryan189@gmail.com>
2022-10-01 21:53:26 +0530

    Iterate over objects

--- tg_notify.sh
+++ tg_notify.sh
@@ -7,17 +7,16 @@ if ! [[ "${CHANGED_FILE}" =~ "json" ]]; then
     exit 0
 fi
 
-DEVICE="$(jq -r '.device' "${CHANGED_FILE}")"
+DEVICE="$(jq -r '.[] | .device' "${CHANGED_FILE}")"
 STICKER="sticker.tgs"
 PHOTO="banner.jpeg"
-BUILD_DATE="$(date +'%d-%b-%Y' -d @$(jq .datetime ${CHANGED_FILE}))"
-CHANGELOG="${DEVICE_CHANGELOG_URL}${DEVICE}/$(jq -r '.filename' "${CHANGED_FILE}")"
-SFLINK="$(jq -r '.url' "${CHANGED_FILE}")"
-DEVICE_NAME="$(jq -r '.device_name' "${CHANGED_FILE}")"
-PEXV="$(jq -r '.pexv' "${CHANGED_FILE}")"
-XDA="$(jq -r '.xda_thread' "${CHANGED_FILE}")"
-USERNAME="$(jq -r '.tg_username' "${CHANGED_FILE}")"
-DEVICE="$(jq -r '.device' "${CHANGED_FILE}")"
+BUILD_DATE="$(date +'%d-%b-%Y' -d @$(jq '.[] | .datetime' ${CHANGED_FILE}))"
+CHANGELOG="${DEVICE_CHANGELOG_URL}${DEVICE}/$(jq -r '.[] | .filename' "${CHANGED_FILE}")"
+SFLINK="$(jq -r '.[] | .url' "${CHANGED_FILE}")"
+DEVICE_NAME="$(jq -r '.[] | .device_name' "${CHANGED_FILE}")"
+PEXV="$(jq -r '.[] | .pexv' "${CHANGED_FILE}")"
+XDA="$(jq -r '.[] | .xda_thread' "${CHANGED_FILE}")"
+USERNAME="$(jq -r '.[] | .tg_username' "${CHANGED_FILE}")"
 ROM="https://github.com/PixelExtended/OTA/blob/snow/changelog.md"
 DONATION="https://heisinbug.tech/pay"
 
