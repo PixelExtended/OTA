fd3bc4522e71e056eddb0d47b55a5f5aff05e08a
Immanuel Raj <iamimmanuelraj@gmail.com>
2022-12-02 23:53:40 +0530

    OTA: Update for new OTA updater

--- support/ota.py
+++ support/ota.py
@@ -1,4 +1,4 @@
-# Authored By : @https://t.me/Immanuel_Raj
+# Authored By : @https://t.me/iamimmanuelraj
 
 # Imports
 import os
@@ -28,6 +28,8 @@ tgname=input("\nEnter Your telegram username [Without '@'] : ")
 codename=input("\nEnter your device code name :- eg miatoll/laurel_sprout : ")
 device=input("\nEnter your device full name :- eg Poco M2 Pro : ")
 xda=input("\nEnter Your device's XDA post link : ")
+ghun=input("\nEnter Your Github username : ")
+name=input("\nEnter Your Name : ")
 
 print ("These Inputs are For SourceForge Uploading, you will be asked password just after your sourceforge username")
 sf=input("Enter Your SourceForge Username ")
@@ -36,4 +38,18 @@ sf=input("Enter Your SourceForge Username ")
 os.system("scp out/target/product/%s/PixelExtended*.zip %s@frs.sourceforge.net://home/frs/project/pixelextended/%s/"%(codename,sf,codename))
 
 # OTA/TG
-os.system("bash OTA/support/ota.sh '%s' '%s' '%s' '%s'"%(codename,tgname,device,xda))
+os.system("bash OTA/support/ota.sh '%s' '%s' '%s' '%s' '%s' '%s'"%(codename,tgname,device,xda,ghun,name))
+
+# Open json for formatting
+init = open("OTA/builds/%s.json"%(codename), "rt")
+#output file to write the result to
+fout = open("OTA/builds/%s_temp.json"%(codename), "wt")
+#for each line in the input file
+for line in init:
+	#read replace the string and write to output file
+	fout.write(line.replace('/', '\/'))
+#close input and output files
+os.system("rm -rf  OTA/builds/%s.json"% (codename))
+os.system("mv OTA/builds/%s_temp.json OTA/builds/%s.json"% (codename,codename))
+init.close()
+fout.close()
--- support/ota.sh
+++ support/ota.sh
@@ -2,6 +2,8 @@ DEVICE=$1
 TG_USERNAME=$2
 DEVICE_NAME=$3
 XDA_THREAD=$4
+GHUN=$5
+NAME=$6
 
 DATETIME=$(grep "org.pixelexperience.build_date_utc=" out/target/product/$DEVICE/system/build.prop | cut -d "=" -f 2)
 FILENAME=$(find out/target/product/$DEVICE/PixelExtended*.zip | cut -d "/" -f 5)
@@ -9,12 +11,15 @@ ID=$(sha256sum out/target/product/$DEVICE/PixelExtended*.zip | cut -d " " -f 1)
 FILEHASH=$(md5sum out/target/product/$DEVICE/PixelExtended*.zip | cut -d " " -f 1)
 SIZE=$(wc -c out/target/product/$DEVICE/PixelExtended*.zip | awk '{print $1}')
 URL="https://sourceforge.net/projects/pixelextended/files/$DEVICE/$FILENAME/download"
-VERSION="13"
+VERSION=$(grep "org.pixelexperience.version=" out/target/product/$DEVICE/system/build.prop | cut -d "=" -f 2)
 STATUS="Active"
 PEXV=$(grep "org.pex.version=" out/target/product/$DEVICE/system/build.prop | cut -d "=" -f 2)
-JSON_FMT='[\n{\n"error":false,\n"filename": "%s",\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"status": "%s", \n"pexv": "%s", \n"id": "%s", \n"tg_username": "%s", \n"device_name":"%s", \n"device":"%s", \n"xda_thread":"%s"\n}\n]'
+DONATE_URL="https://heisinbug.tech/pay"
+WEBSITE_URL="https://pixelextended.tech"
+NEWS_URL="https://t.me/pexupdates"
+JSON_FMT='{\n\t"error":false,\n\t"filename":"%s",\n\t"datetime":%s,\n\t"size":%s,\n\t"url":"%s",\n\t"filehash":"%s",\n\t"version":"%s",\n\t"status":"%s",\n\t"pexv":"%s",\n\t"id":"%s",\n\t"tg_username":"%s",\n\t"device_name":"%s",\n\t"device":"%s",\n\t"xda_thread":"%s",\n\t"maintainers": [{\n\t\t"main_maintainer":false,\n\t\t"github_username":"%s",\n\t\t"name":"%s"\n\t}],\n\t"donate_url":"%s",\n\t"website_url":"%s",\n\t"news_url":"%s",\n\t"forum_url":"%s"\n}'
 
-printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$STATUS" "$PEXV" "$ID" "$TG_USERNAME" "$DEVICE_NAME" "$DEVICE" "$XDA_THREAD" > OTA/builds/$DEVICE.json
+printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$STATUS" "$PEXV" "$ID" "$TG_USERNAME" "$DEVICE_NAME" "$DEVICE" "$XDA_THREAD" "$GHUN" "$NAME" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$XDA_THREAD" > OTA/builds/$DEVICE.json
 echo OTA/builds/$DEVICE.json file created
 
 BUILD_DATE=$(echo $FILENAME | cut -d "-" -f 3)
