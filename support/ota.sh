9017192a684858123ccfac20e503d9ca0239edc0
Immanuel Raj <iamimmanuelraj@gmail.com>
2022-12-05 22:10:59 +0530

    OTA: Update for New Updater and Notifier stuff

--- support/ota.sh
+++ support/ota.sh
@@ -17,7 +17,7 @@ PEXV=$(grep "org.pex.version=" out/target/product/$DEVICE/system/build.prop | cu
 DONATE_URL="https://heisinbug.tech/pay"
 WEBSITE_URL="https://pixelextended.github.io/"
 NEWS_URL="https://t.me/pexupdates"
-JSON_FMT='[\n\t{\n\t"error":false,\n\t"filename":"%s",\n\t"datetime":%s,\n\t"size":%s,\n\t"url":"%s",\n\t"filehash":"%s",\n\t"version":"%s",\n\t"status":"%s",\n\t"pexv":"%s",\n\t"id":"%s",\n\t"tg_username":"%s",\n\t"device_name":"%s",\n\t"device":"%s",\n\t"xda_thread":"%s",\n\t"maintainers": [{\n\t\t"main_maintainer":false,\n\t\t"github_username":"%s",\n\t\t"name":"%s"\n\t}],\n\t"donate_url":"%s",\n\t"website_url":"%s",\n\t"news_url":"%s",\n\t"forum_url":"%s"\n\t}\n\]'
+JSON_FMT='{\n\t"error":false,\n\t"filename":"%s",\n\t"datetime":%s,\n\t"size":%s,\n\t"url":"%s",\n\t"filehash":"%s",\n\t"version":"%s",\n\t"status":"%s",\n\t"pexv":"%s",\n\t"id":"%s",\n\t"tg_username":"%s",\n\t"device_name":"%s",\n\t"device":"%s",\n\t"xda_thread":"%s",\n\t"maintainers": [{\n\t\t"main_maintainer":false,\n\t\t"github_username":"%s",\n\t\t"name":"%s"\n\t}],\n\t"donate_url":"%s",\n\t"website_url":"%s",\n\t"news_url":"%s",\n\t"forum_url":"%s"\n}'
 
 printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$STATUS" "$PEXV" "$ID" "$TG_USERNAME" "$DEVICE_NAME" "$DEVICE" "$XDA_THREAD" "$GHUN" "$NAME" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$XDA_THREAD" > OTA/builds/$DEVICE.json
 echo OTA/builds/$DEVICE.json file created
--- tg_notify.sh
+++ tg_notify.sh
@@ -7,17 +7,17 @@ if ! [[ "${CHANGED_FILE}" =~ "json" ]]; then
     exit 0
 fi
 
-STATUS="$(jq -r '.[] | .status' "${CHANGED_FILE}")"
-DEVICE="$(jq -r '.[] | .device' "${CHANGED_FILE}")"
+STATUS="$(jq -r '.status' "${CHANGED_FILE}")"
+DEVICE="$(jq -r '.device' "${CHANGED_FILE}")"
 STICKER="sticker.tgs"
 PHOTO="banner.jpeg"
-BUILD_DATE="$(date +'%d-%b-%Y' -d @$(jq '.[] | .datetime' ${CHANGED_FILE}))"
-CHANGELOG="${DEVICE_CHANGELOG_URL}${DEVICE}/$(jq -r '.[] | .filename' "${CHANGED_FILE}")"
-SFLINK="$(jq -r '.[] | .url' "${CHANGED_FILE}")"
-DEVICE_NAME="$(jq -r '.[] | .device_name' "${CHANGED_FILE}")"
-PEXV="$(jq -r '.[] | .pexv' "${CHANGED_FILE}")"
-XDA="$(jq -r '.[] | .xda_thread' "${CHANGED_FILE}")"
-USERNAME="$(jq -r '.[] | .tg_username' "${CHANGED_FILE}")"
+BUILD_DATE="$(date +'%d-%b-%Y' -d @$(jq '.datetime' ${CHANGED_FILE}))"
+CHANGELOG="${DEVICE_CHANGELOG_URL}${DEVICE}/$(jq -r '.filename' "${CHANGED_FILE}")"
+SFLINK="$(jq -r '.url' "${CHANGED_FILE}")"
+DEVICE_NAME="$(jq -r '.device_name' "${CHANGED_FILE}")"
+PEXV="$(jq -r '.pexv' "${CHANGED_FILE}")"
+XDA="$(jq -r '.xda_thread' "${CHANGED_FILE}")"
+USERNAME="$(jq -r '.tg_username' "${CHANGED_FILE}")"
 ROM="https://github.com/PixelExtended/OTA/blob/trece/changelog.md"
 DONATION="https://heisinbug.tech/pay"
 if [[ $STATUS = "Active" ]]
